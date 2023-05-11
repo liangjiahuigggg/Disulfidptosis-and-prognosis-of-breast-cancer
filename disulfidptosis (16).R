3705cd6a1fa84bd440ed0d61df3e0b8eb9841081
liangjiahuigggg <133225998+liangjiahuigggg@users.noreply.github.com>
2023-05-12 01:02:45 +0800

    Update disulfidptosis (16).R

--- disulfidptosis (16).R
+++ disulfidptosis (16).R
@@ -48,10 +48,5 @@ ggplot(corLodes, aes(x = x, stratum = stratum, alluvium = Cohort,fill = stratum,
 dev.off()
 
 
-######??????Ñ§??: https://www.biowolf.cn/
-######?Î³?Á´??1: https://shop119322454.taobao.com
-######?Î³?Á´??2: https://ke.biowolf.cn
-######?Î³?Á´??3: https://ke.biowolf.cn/mobile
-######?â¿¡??Ê¦????: seqbio@foxmail.com
-######?â¿¡??Ê¦Î¢??: eduBio
+
 
